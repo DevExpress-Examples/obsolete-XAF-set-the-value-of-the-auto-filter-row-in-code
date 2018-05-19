e662e00b52c23082a326fd4ef2e6fa8050427ef6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/VB/WebSample.Module/Updater.vb
@@ -0,0 +1,33 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports DevExpress.Data.Filtering
+Imports WinSample.Module
+
+Namespace WebSample.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal objectSpace As IObjectSpace, ByVal currentDBVersion As Version)
+			MyBase.New(objectSpace, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+			CreatePerson("Sam", "USA")
+			CreatePerson("John", "USA")
+			CreatePerson("Keith", "UK")
+			CreatePerson("Gloria", "UK")
+		End Sub
+		Private Sub CreatePerson(ByVal name As String, ByVal country As String)
+			Dim person As Person = ObjectSpace.FindObject(Of Person)(New BinaryOperator("Name", name))
+			If person Is Nothing Then
+				person = ObjectSpace.CreateObject(Of Person)()
+				person.Name = name
+				person.Country = country
+				person.Save()
+			End If
+		End Sub
+	End Class
+End Namespace
