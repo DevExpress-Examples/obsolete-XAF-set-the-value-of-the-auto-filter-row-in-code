9137e1e9e19c7753cf14461d45e30b47aa2ad0d7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/WebSampleVB/WebSample.Module.Web/FilterRowController.vb
@@ -0,0 +1,38 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Text
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.Web.Editors.ASPx
+Imports WinSample.Module
+Imports DevExpress.ExpressApp.NodeWrappers
+Imports System.Net
+
+Namespace WebSample.Module.Web
+	Partial Public Class FilterRowController
+		Inherits ViewController
+		Public Sub New()
+			InitializeComponent()
+			RegisterActions(components)
+			TargetViewId = "Person_ListView"
+		End Sub
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			Dim wrapper As ListViewInfoNodeWrapper = (TryCast(View, ListView)).Model
+			If wrapper.FilterExpression = String.Empty Then
+				wrapper.FilterExpression = "[Country] = 'USA'"
+			End If
+		End Sub
+		Private Sub changeFilterAction_Execute(ByVal sender As Object, ByVal e As SimpleActionExecuteEventArgs) Handles changeFilterAction.Execute
+			If View.IsControlCreated Then
+				Dim control As ASPxGridControl = TryCast((TryCast(View, ListView)).Editor.Control, ASPxGridControl)
+				control.FilterExpression = "[Country] = 'UK'"
+			End If
+		End Sub
+	End Class
+End Namespace
